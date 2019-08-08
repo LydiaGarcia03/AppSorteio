f41e081149c4a9a579786c75a7c2321c46de8889
Lydia <lydiagarcia.chan@gmail.com>
2019-08-08 11:24:55 -0300

    App Toast e Log | Sem Sorteio

--- app/src/main/java/br/ifsc/edu/applinearlayout/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/applinearlayout/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("CicloDeVida", "OnCreate" ); // Prova real de em que parte do ciclo de vida o code está
+        Log.i("CicloDeVida", "OnCreate"); // Prova real de em que parte do ciclo de vida o code está
         Toast.makeText(getApplicationContext(), "OnCreate", Toast.LENGTH_LONG).show();
     }
 
