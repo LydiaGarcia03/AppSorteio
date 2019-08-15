ca629603cc88f1df416187a5b0459e87f280a347
Lydia <lydiagarcia.chan@gmail.com>
2019-08-15 09:16:11 -0300

    Sorteio em andamento Erros na exibição do número aleatório

--- app/src/main/java/br/ifsc/edu/applinearlayout/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/applinearlayout/MainActivity.java
@@ -3,7 +3,11 @@ package br.ifsc.edu.applinearlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.i("CicloDeVida", "OnCreate"); // Prova real de em que parte do ciclo de vida o code está
         Toast.makeText(getApplicationContext(), "OnCreate", Toast.LENGTH_LONG).show();
     }
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
         Log.i("CicloDeVida", "onPause" );
         Toast.makeText(getApplicationContext(), "OnPause", Toast.LENGTH_LONG).show();
     }
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
         Log.i("CicloDeVida", "OnResume" );
         Toast.makeText(getApplicationContext(), "OnResume", Toast.LENGTH_LONG).show();
     }
@@ -40,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
+
         Log.i("CicloDeVida", "OnStop" );
         Toast.makeText(getApplicationContext(), "OnStop", Toast.LENGTH_LONG).show();
     }
@@ -47,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
+
         Log.i("CicloDeVida", "OnRestart" );
         Toast.makeText(getApplicationContext(), "OnRestart", Toast.LENGTH_LONG).show();
     }
@@ -54,7 +63,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
         Log.i("CicloDeVida", "OnDestroy" );
         Toast.makeText(getApplicationContext(), "OnDestroy", Toast.LENGTH_LONG).show();
+    }
+
+    public void sortear(View view) {
+        // Todo metodo do xml tem que receber uma view
+        int numRandom = new Random().nextInt(101);
+        findViewById(R.id.txtNumRandom).setText(numRandom.toString);
+
+        int numUser =  Integer.parseInt(((EditText) findViewById(R.id.txtNumUser)).getText().toString()) ;
+
+        if(numRandom == numUser){
+
+            Toast.makeText(getApplicationContext(), "Número igual", Toast.LENGTH_LONG).show();
+
+        }
+
     }
 }
